d760751d4e5f01c3ab058788b5755b10ed2c800f
qianfei11 <490584635@qq.com>
2020-11-08 14:29:28 +0800

    remove ZJGSUCTF-2018
    move the challenges to https://github.com/ZJGSIS/ZJGSUCTF-Challenges

--- platform/ZJGSCTF/ZJGSCTF2018/pwn/windows2k-4/mylib.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-int changePrivByUname(char *username);
-void perrorAndExit(char* s);
-void handler(int signum);
-int readAInt();
-void setNoBufAndTimeout(int time);
-int readNbytes(char*s ,size_t len,char endC);
-int readNbytesEndWith0A(char* s,size_t len);
